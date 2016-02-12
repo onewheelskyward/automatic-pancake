13c48ed055b9c74bf5aad776fe8c1438839e213c
Andrew Kreps <andrew.kreps@gmail.com>
2016-02-12 14:40:54 -0800

    Turns out :'s aren't valid in filenames.

--- server/server.js
+++ server/server.js
@@ -121,7 +121,7 @@ function getTitle(youtubeId, callback) {
     titleCmd = "youtube-dl -e " + youtubeId;
     console.log("executing " + titleCmd);
     exec(titleCmd, function(error, stdout, stderr) {
-        callback(stdout.trim());
+        callback(stdout.trim().replace(':', ''));
     });
 }
 
