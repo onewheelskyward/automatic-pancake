336ace3b521adbcada15816415c13e7950a0d570
Andrew Kreps <andrew.kreps@gmail.com>
2016-04-19 14:37:37 -0700

    Adding logging.

--- server/delete.js
+++ server/delete.js
@@ -7,7 +7,9 @@ module.exports = function(app, config, r) {
         r.db(config.database).table('files').get(req.params.id).run().then(function(result) {
             //filter(r.row('id').eq(id))
             var fullpath = __dirname + '/files/';
+            console.log("Removing " + fullpath);
             fs.unlink(fullpath, function() {
+                console.log("Deleting " + req.params.id);
                 r.db(config.database).table('files').get(req.params.id).delete().run();
             });
         });
