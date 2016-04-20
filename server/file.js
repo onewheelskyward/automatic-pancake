bb3b28c827c0757e14dd28f2b84e24114dc3f13e
Andrew Kreps <andrew.kreps@gmail.com>
2016-04-20 22:41:31 +0000

    Added ability to pull files by type.

--- server/file.js
+++ server/file.js
@@ -5,5 +5,10 @@ module.exports = function(app, config, r) {
             res.send(result);
         });
     });
+    app.get('/files/:type', function (req, res) {
+        r.db(config.database).table('files').filter({'type': req.params.type}).orderBy(r.desc('created')).run().then(function(result) {
+            res.send(result);
+        });
+    });
     // TODO: Add file updates and deletes here
 };
